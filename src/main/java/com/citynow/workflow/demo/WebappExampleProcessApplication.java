b8287a4747ef2cf5635c9b2ef8c06942ffac45d7
chucuabac <ngochien96pt@gmail.com>
2020-04-09 09:36:52 +0700

    tạo project và thêm một số dependency

---
+++ src/main/java/com/citynow/workflow/demo/WebappExampleProcessApplication.java
@@ -0,0 +1,11 @@
+package com.citynow.workflow.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappExampleProcessApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebappExampleProcessApplication.class, args);
+	}
+}
